f2f236af32b02551b331ef0cc767e42daaa35b51
Jake Soenneker <jake@soenneker.com>
2024-09-09 17:04:26 -0500

    Performance

--- src/ByteArrayExtension.cs
+++ src/ByteArrayExtension.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Soenneker.Extensions.ByteArray;
@@ -10,19 +10,36 @@ namespace Soenneker.Extensions.ByteArray;
 public static class ByteArrayExtension
 {
     /// <summary>
-    /// Shorthand for <code>Encoding.UTF8.GetString</code>
+    /// Converts the specified byte array to a UTF-8 encoded string.
     /// </summary>
+    /// <param name="value">The byte array to convert.</param>
+    /// <returns>A string representation of the byte array, decoded using UTF-8 encoding.</returns>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToStr(this byte[] value)
     {
-        string result = Encoding.UTF8.GetString(value);
-        return result;
+        return Encoding.UTF8.GetString(value);
     }
 
+    /// <summary>
+    /// Converts the specified byte array to a hexadecimal string representation.
+    /// </summary>
+    /// <param name="value">The byte array to convert.</param>
+    /// <returns>A string containing the hexadecimal representation of the byte array.
+    /// If the array is empty, an empty string is returned.</returns>
     [Pure]
     public static string ToHex(this byte[] value)
     {
-        string result = BitConverter.ToString(value).Replace("-", "");
-        return result;
+        if (value.Length == 0)
+            return "";
+
+        var hex = new StringBuilder(value.Length * 2);
+
+        foreach (byte b in value)
+        {
+            hex.Append(b.ToString("x2"));
+        }
+
+        return hex.ToString();
     }
 }
